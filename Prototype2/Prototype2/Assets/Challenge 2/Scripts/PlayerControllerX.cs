32101c3d8fbbd1aa219e1504267fcb5d81227bf4
JoseNogueira13 <92999470+JoseNogueira13@users.noreply.github.com>
2025-03-13 17:44:28 +0000

    .

---
+++ Prototype2/Prototype2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float cooldownTime = 1.0f;
+    private float nextFireTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFireTime)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                nextFireTime = Time.time + cooldownTime;
+            }
+        }
+    }
+}
---
+++ Prototype2/Prototype2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float speed = 25;
+    public float xRange = 15;
+    public GameObject projectilePrefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
